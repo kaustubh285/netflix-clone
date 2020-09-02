65de7a4694f4ce95bdcfeae8a82c608300877f58
kaustubh <kaustubh285@gmail.com>
2020-09-02 16:42:49 +0530

    Icon, favicon changed, manifest updated

--- src/components/Row.js
+++ src/components/Row.js
@@ -3,12 +3,14 @@ import axios from "../axios";
 import "./row.css";
 import YouTube from "react-youtube";
 import movieTrailer from "movie-trailer";
+import Alert from "@material-ui/lab/Alert";
 
 const imgUrl = "https://image.tmdb.org/t/p/original/";
 
 function Row({ title, fetchUrl, isPoster }) {
   const [movies, setMovies] = useState([]);
   const [trailerUrl, setTrailerUrl] = useState("");
+  const [trailerNotFound, setTrailerNotFound] = useState(false);
 
   useEffect(() => {
     async function fetchData() {
@@ -30,15 +32,20 @@ function Row({ title, fetchUrl, isPoster }) {
   };
 
   const handleClick = (movie) => {
-    if (trailerUrl) {
+    if (trailerUrl || trailerNotFound) {
       setTrailerUrl("");
+      setTrailerNotFound(false);
     } else {
       movieTrailer(movie?.name || movie?.title || movie.original_name || "")
         .then((url) => {
           const urlParams = new URLSearchParams(new URL(url).search);
           setTrailerUrl(urlParams.get("v"));
+          setTrailerNotFound(false);
         })
-        .catch((error) => console.log(error));
+        .catch((error) => {
+          setTrailerNotFound(true);
+          console.log(error);
+        });
     }
   };
 
@@ -62,6 +69,9 @@ function Row({ title, fetchUrl, isPoster }) {
         ))}
       </div>
       {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
+      {trailerNotFound && (
+        <Alert severity='error'>Sorry! Trailer not found</Alert>
+      )}
     </div>
   );
 }
