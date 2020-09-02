4ae5c41a90f5a1e3b7872d4e602706a32139e499
kaustubh <kaustubh285@gmail.com>
2020-09-02 19:19:50 +0530

    Manifest updated, descp background,Author added

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -23,11 +23,24 @@ function Nav() {
         src='https://www.clipartmax.com/png/full/215-2154144_transparent-background-netflix-logo.png'
         alt='netflix logo'
       />
-      <img
-        className='avatar'
-        src='https://mir-s3-cdn-cf.behance.net/project_modules/disp/366be133850498.56ba69ac36858.png'
-        alt='avatar'
-      />
+      <p className={`author ${show && " author_dark"}`}>
+        <a
+          href='https://github.com/kaustubh285'
+          target='_blank'
+          rel='noopener noreferrer'>
+          By Kaustubh Deshpande
+        </a>
+      </p>
+      <a
+        href='https://github.com/kaustubh285'
+        target='_blank'
+        rel='noopener noreferrer'>
+        <img
+          className='avatar'
+          src='https://mir-s3-cdn-cf.behance.net/project_modules/disp/366be133850498.56ba69ac36858.png'
+          alt='avatar'
+        />
+      </a>
     </div>
   );
 }
